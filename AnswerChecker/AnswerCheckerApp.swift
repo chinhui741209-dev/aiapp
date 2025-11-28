a2dbeb68d96de38c2e2e36afb752dfc4a69e96f1
Joey <s279231@msn.com>
2025-11-28 09:01:24 +0800

    Initial Commit

---
+++ AnswerChecker/AnswerCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnswerCheckerApp.swift
+//  AnswerChecker
+//
+//  Created by Joey Lin on 2025/11/28.
+//
+
+import SwiftUI
+
+@main
+struct AnswerCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnswerCheckerTests/AnswerCheckerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnswerCheckerTests.swift
+//  AnswerCheckerTests
+//
+//  Created by Joey Lin on 2025/11/28.
+//
+
+import Testing
+@testable import AnswerChecker
+
+struct AnswerCheckerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
